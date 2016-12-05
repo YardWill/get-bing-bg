78ac16d77ac30d13508603571130b5af14487b07
翁益栋 <yard@wengyidongdeMacBook-Pro.local>
2016-12-05 10:57:09 +0800

    Mon Dec 05 2016 10:57:09 GMT+0800 (CST)

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ rule.hour = 8;
 rule.minute = 0;
 
 // const times = [];
-// for (let i = 1; i < 60; i++) {
+// for (let i = 1; i < 2; i++) {
 //     times.push(i);
 // }
 // rule.second = times;
 
-schedule.scheduleJob(rule, () => {
+// schedule.scheduleJob(rule, () => {
     request.get('http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN', (error, response, body) => {
         const img = JSON.parse(body).images[0];
         const arr = img.url.split('/');
         const str = arr[arr.length - 1];
-        request(img.url).pipe(fs.createWriteStream(path.join('../myapp/public/bingBG', str)));
+        request(img.url).pipe(fs.createWriteStream(path.join('../myapp/public/img', str)));
         // '../../../Documents/bg'可以选择自己想要保存的目录（相对路径）
         require('./src/cmd.js');
         console.log(`${new Date()}${str} is ok!`);
     });
-});
+// });
