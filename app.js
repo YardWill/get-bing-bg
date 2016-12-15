38d668d851d138741a803aa4d396273cbe08e3ff
翁益栋 <yard@wengyidongdeMacBook-Pro.local>
2016-12-15 10:46:20 +0800

    ss

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const schedule = require('node-schedule');
 
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0, new schedule.Range(1, 6)];
-rule.hour = 8;
-rule.minute = 0;
+rule.hour = 10;
+rule.minute = 46;
 
 schedule.scheduleJob(rule, () => {
+    console.log('start');
     require('./src/get.js');
+    console.log('end');
 });
