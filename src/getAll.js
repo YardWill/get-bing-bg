e4bd7b5a3a7eb289269f5445c0b0422f4c9622e8
yard <zjwengyidong@outlook.com>
2016-12-19 15:59:58 +0800

    历史壁纸更新

--- src/getAll.js
+++ src/getAll.js
@@ -2,17 +2,32 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 
-request.get('http://bing.ioliu.cn/v1?p=1&size=287', (error, response, body) => {
-    const data = JSON.parse(body);
-    if (data.status.code === 200) {
-        data.data.forEach((e) => {
-            const img = e.url;
-            const arr = img.split('/');
-            const str = arr[arr.length - 1];
-            request(img).pipe(fs.createWriteStream(path.join('./img', str)));
-            console.log(`${new Date()}${str} is ok!`);
-        });
-    } else {
-        console.log('接口请求未成功');
-    }
-});
+let i = 0;
+
+const getImg = (index, number = 5) => {
+    request.get(`http://bing.ioliu.cn/v1?p=${index}&size=${number}`, (error, response, body) => {
+        const data = JSON.parse(body);
+        if (data.status.code === 200) {
+            data.data.forEach((e) => {
+
+                const img = e.url;
+                const arr = img.split('/');
+                const str = arr[arr.length - 1];
+                const url = `http://images.ioliu.cn/bing/${str}`;
+                request(url)
+                    .pipe(fs.createWriteStream(path.join('./img', str))
+                        .on('close', () => {
+                            i++;
+                            console.log(`${i}${new Date()}${str} is ok!`);
+                            if (i % number === 0) {
+                                getImg((i / number) + 2);
+                            }
+                        }));
+            });
+        } else {
+            console.log('下载完成');
+        }
+    });
+};
+
+getImg(i);
